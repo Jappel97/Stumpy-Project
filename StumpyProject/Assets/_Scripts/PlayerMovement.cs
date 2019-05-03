ada395bf13f9d5d1f69d0ac3bafa2ca62ba98a1d
Jappel97 <32598921+Jappel97@users.noreply.github.com>
2019-05-03 16:46:05 -0500

    Initial Re-Commit

---
+++ StumpyProject/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.gameObject.transform.position = new Vector3(this.transform.position.x + speed, this.transform.position.y);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            this.gameObject.transform.position = new Vector3(this.transform.position.x - speed, this.transform.position.y);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            this.gameObject.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 10);
+        }
+    }
+}
