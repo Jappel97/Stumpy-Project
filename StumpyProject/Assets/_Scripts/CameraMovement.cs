a2724f5d1b7f76dd122362606e9fb5bb9250b387
Jappel97 <32598921+Jappel97@users.noreply.github.com>
2019-09-20 16:44:03 -0500

    Implemented basic camera script

---
+++ StumpyProject/Assets/_Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public GameObject myPlayer;
+    
+
+    private void LateUpdate()
+    {
+        this.gameObject.transform.position = new Vector3(myPlayer.transform.position.x, this.transform.position.y, this.transform.position.z);
+    }
+    
+}
